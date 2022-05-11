c846b386ab031a046e2d55b0759ebb388d97bb98
Simone Malesardi <simomale98@gmail.com>
2022-05-11 15:36:30 +0200

    aggiunta deserializzazione

---
+++ deserialization.py
@@ -0,0 +1,23 @@
+import pickle
+import pandas as pd
+import os
+import csv
+
+folder = './simulated-data-raw-github/'
+files = []
+
+with os.scandir(folder) as entries:
+    for entry in entries:
+        name = entry.name
+        files.append(name)
+
+files.sort()
+for entry in files: 
+    name = folder+entry
+    with open(name, 'rb') as f:
+        newName = entry.replace(".pkl", ".csv")
+        #data = pickle.load(f)
+        #nome = './csv-github/'+entry.name.replace(".pkl", ".csv")
+        #df = pd.DataFrame(data)
+        #df.to_csv(nome, index=False)
+ 
