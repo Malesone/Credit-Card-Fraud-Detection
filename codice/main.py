a9a1209af841bb42b3a91277a84fe209cd46c5f5
Simone Malesardi <simomale98@gmail.com>
2022-06-07 17:27:42 +0200

    decommento

--- codice/main.py
+++ codice/main.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     #d.generate_dataset(n_customers = 100, n_terminals = 10, nb_days = 10, radius = 5) 
     d.read_dataset() 
     
-    #load = Statistic(type = Operation.generation.value)
-    #app = App()
-    #app.delete_all()
+    load = Statistic(type = Operation.generation.value)
+    app = App()
+    app.delete_all()
     
-    #app.create_all(d.customers.dataset, d.terminals.dataset, d.transactions.dataset)
-    #load.stop_time()
+    app.create_all(d.customers.dataset, d.terminals.dataset, d.transactions.dataset)
+    load.stop_time()
 
     """extension = Statistic(type = Operation.extension.value)
     app.extension()
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     app.transactions_per_period()
     tpp.stop_time()
     """
-    #app.close()
+    app.close()
 
     #d.statistics.extend([load, extension, tpp])
-    #d.statistics.append(load)
-    #d.gen_plot()
+    d.statistics.append(load)
+    d.gen_plot()
